cd6ae65ca6ab241890c9d03cf48b73a022021d6c
bangyewon <yewon9272@duksung.ac.kr>
2024-05-03 23:06:32 +0900

    초기환경 셋팅

---
+++ src/main/java/com/ITTAxMED/hackathonTeam3/HackathonTeam3Application.java
@@ -0,0 +1,13 @@
+package com.ITTAxMED.hackathonTeam3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonTeam3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonTeam3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ITTAxMED/hackathonTeam3/HackathonTeam3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ITTAxMED.hackathonTeam3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonTeam3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
